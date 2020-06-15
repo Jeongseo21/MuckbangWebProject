ac8c4757158ab1b144e6046aefa3a4bdccb0624e
한정서 <61036124+Jeongseo21@users.noreply.github.com>
2020-06-16 08:55:53 +0900

    Update youtube_videos.py

--- myProject(Dangole)/youtube_videos.py
+++ myProject(Dangole)/youtube_videos.py
@@ -9,7 +9,7 @@ from oauth2client.tools import argparser
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyC567HkL6VbZhjGxugUXZbBJjvHalOYnsc"
+DEVELOPER_KEY = ""
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
