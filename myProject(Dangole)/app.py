c242c5d5c686039e45d855e49ae33c0fb58bc9c7
jeongseo han <jeongseo21@gmail.com>
2020-06-17 21:17:15 +0900

    에러없이 뼈대만 있는 상태

--- myProject(Dangole)/app.py
+++ myProject(Dangole)/app.py
@@ -15,12 +15,12 @@ def videos():
   return render_template('list.html')
 
 @app.route('/list',methods=['POST'])
-def videospost():
-  youtubes = youtube_videos.youtube_search()
-  print("\n\nVideos:\n\n", "\n\n".join(youtubes['videos']), "\n")
-  print("\n\nChannels:\n\n", "\n".join(youtubes['channels']), "\n")
-  print("\n\nPlaylists:\n\n", "\n".join(youtubes['playlists']), "\n")
-  return jsonify({'result':'success','videos':youtubes})
+# def videospost():
+#   youtubes = youtube_videos.youtube_search()
+#   print("\n\nVideos:\n\n", "\n\n".join(youtubes['videos']), "\n")
+#   print("\n\nChannels:\n\n", "\n".join(youtubes['channels']), "\n")
+#   print("\n\nPlaylists:\n\n", "\n".join(youtubes['playlists']), "\n")
+#   return jsonify({'result':'success','videos':youtubes})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
